ad7901509def1b686e25c5a6f2f84f7ae87688a1
tp00012x <anthon_torres01@yahoo.com>
2017-08-25 15:41:17 -0500

    Second commit

--- beer_tracker/urls.py
+++ beer_tracker/urls.py
@@ -5,6 +5,5 @@ from beer_tracker import views
 app_name ='beer_tracker'
 
 urlpatterns = [
-    # url(r'^register/$', views.register, name='register'),
     url(r'^user_login/$', views.user_login, name="user_login"),
 ]
